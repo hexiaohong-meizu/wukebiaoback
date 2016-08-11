2f8fb4df80bf0aa2b5b5cd2b28fc07b81120c77d
hexiaohong <hexiaohong@meizu.com>
2016-08-11 11:59:51 +0800

    update two file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("my name is hexiaohong");
-		System.out.println("my age is 55");
+		System.out.println("my age is 666");
 		}
 	}
